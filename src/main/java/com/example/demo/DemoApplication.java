553736a520d258b9a61c6b05b7cf96ce58e93d4f
ch <mkmsacc@outlook.com>
2023-07-29 18:47:15 +0700

    Add tcp server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.tcp.TcpServer;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Value("${tcp.server.port}")
+    private int port;
+
+    @Bean
+    ApplicationRunner run() {
+        return args -> TcpServer.create()
+                .port(port)
+                .wiretap(true)
+                .handle((nettyInbound, nettyOutbound) -> nettyOutbound.sendByteArray(nettyInbound.receive()
+                        .asByteArray()
+                ))
+                .bindNow()
+                .onDispose()
+                .block();
+
+    }
 
 }
